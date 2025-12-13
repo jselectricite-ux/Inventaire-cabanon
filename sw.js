da8e67f220e7255e25b7ff70d55fe5d0dfac0a8d
jselectricite-ux <js.electricite@outlook.fr>
2025-12-13 14:21:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,39 @@
+const CACHE_NAME = "inventaire-cabanon-v1";
 
-const CACHE_NAME = 'inventaire-cabanon-v1';
-const FILES_TO_CACHE = [
-  '/index.html',
-  '/manifest.webmanifest',
-  '/css/style.css',
-  '/js/app.js'
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./hager.json",
+  "./legrand.json",
+  "./schneider.json",
+  "./sonepar.json",
+  "./wurth.json",
+  "https://unpkg.com/html5-qrcode"
 ];
 
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => {
-    if (k !== CACHE_NAME) return caches.delete(k);
-  }))));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (evt) => {
-  if (evt.request.method !== 'GET') return;
-  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
 });
